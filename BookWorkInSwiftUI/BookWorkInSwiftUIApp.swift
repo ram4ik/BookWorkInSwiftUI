5b95eb15ce46fc0857aa15cd469a552b96e8742d
ram4ik <ram4ik@hotmail.com>
2023-11-18 15:36:52 +0200

    Initial Commit

---
+++ BookWorkInSwiftUI/BookWorkInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWorkInSwiftUIApp.swift
+//  BookWorkInSwiftUI
+//
+//  Created by admin on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BookWorkInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
